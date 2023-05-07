12ce4a6bc68f94b6334aa015baf7044ac1912e1b
Shahaf15 <shahafpour@gmail.com>
2023-05-07 19:51:16 +0300

    till JWT

---
+++ models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'Please provide name'],
+        minlength: 3, 
+        maxlength: 20, 
+        trim: true,
+    },
+    email: {
+        type: String, 
+        required: [true, 'Please provide email'],
+        validate:{
+            validator: validator.isEmail,
+            message:'Please provide a valid email'
+        },
+        unique: true,
+    },
+    password: {
+        type: String, 
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
+    lasName: {
+        type: String, 
+        maxlength: 20, 
+        trim: true,
+        default: 'lastName'
+    },
+    location: {
+        type: String, 
+        maxlength: 20, 
+        trim: true,
+        default: 'my location'
+    },
+})
+
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+export default mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
+import 'express-async-errors'
 
 // db and authtentucationUser
 import connectDB from './db/connect.js'
