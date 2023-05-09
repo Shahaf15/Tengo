92f57242b43aaecd0aafa5eb54410e57a2e66a67
Shahaf15 <shahafpour@gmail.com>
2023-05-09 18:02:38 +0300

    server to client

--- models/User.js
+++ models/User.js
@@ -1,46 +1,52 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
     name: {
-        type: String, 
+        type: String,
         required: [true, 'Please provide name'],
-        minlength: 3, 
-        maxlength: 20, 
+        minlength: 3,
+        maxlength: 20,
         trim: true,
     },
     email: {
-        type: String, 
+        type: String,
         required: [true, 'Please provide email'],
-        validate:{
+        validate: {
             validator: validator.isEmail,
-            message:'Please provide a valid email'
+            message: 'Please provide a valid email'
         },
         unique: true,
     },
     password: {
-        type: String, 
+        type: String,
         required: [true, 'Please provide password'],
         minlength: 6,
+        select: false,
     },
     lasName: {
-        type: String, 
-        maxlength: 20, 
+        type: String,
+        maxlength: 20,
         trim: true,
         default: 'lastName'
     },
     location: {
-        type: String, 
-        maxlength: 20, 
+        type: String,
+        maxlength: 20,
         trim: true,
         default: 'my location'
     },
 })
 
-UserSchema.pre('save', async function(){
+UserSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
+}
 
 export default mongoose.model('User', UserSchema)
